b04aa1322bd9682e5e8d9bc43ac8985f34decc2a
Muhammad Naeem <naeem0313@gmail.com>
2024-12-01 05:41:29 +0500

    Surface, Shape, Clickable Create and modify Surface to draw background for Composables, add click action to any composable. Set weight or offset modifiers.

---
+++ app/src/main/java/com/naeemdev/jetpackcompose_ui_components_material_3/clicksurfacesandclicks/TutorialHeader.kt
@@ -0,0 +1,23 @@
+package com.naeemdev.jetpackcompose_ui_components_material_3.clicksurfacesandclicks
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TutorialHeader(text: String, modifier: Modifier = Modifier) {
+
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 12.dp, bottom = 6.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        text = text
+    )
+}
